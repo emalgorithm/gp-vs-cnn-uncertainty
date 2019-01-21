2885f7b81b04e6256bf039c53b1367e93894d21a
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-01-21 16:57:23 +0000

    Implement transform to embed images in feature vectors using the pretrained CNN

---
+++ embedding_transform.py
@@ -0,0 +1,13 @@
+from cnn import get_cnn_embedding_model
+import torch
+
+
+class Embedding(object):
+    def __init__(self):
+        self.cnn_model = get_cnn_embedding_model()
+
+    def __call__(self, sample):
+        embedding = self.cnn_model.predict(sample.numpy().reshape(-1, 28, 28, 1))
+        embedding = embedding.reshape((128))
+        embedding = torch.from_numpy(embedding)
+        return embedding
