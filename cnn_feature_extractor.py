d9655fe6fbccee28f5b91c1ef842d66b945ebe7f
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-01-22 15:25:22 +0000

    Remove import loop

--- cnn_feature_extractor.py
+++ cnn_feature_extractor.py
@@ -1,4 +1,4 @@
-from cnn import get_cnn_embedding_model
+from keras.models import load_model
 
 
 class CNNFeatureExtractor:
@@ -6,8 +6,16 @@ class CNNFeatureExtractor:
     CNN feature extractor
     """
     def __init__(self):
-        self.model = get_cnn_embedding_model()
+        self.model = self.get_cnn_embedding_model()
 
     def forward(self, x):
         return self.model.predict(x)
+
+    @staticmethod
+    def get_cnn_embedding_model():
+        cnn_model = load_model('models/CNN_mnist_model.h5')
+        cnn_model.pop()
+        cnn_model.pop()
+
+        return cnn_model
 
