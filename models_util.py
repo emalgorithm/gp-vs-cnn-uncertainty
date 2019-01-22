0a12ac825140cbc3c812fdf65b4d293aee708d2d
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-01-22 22:21:54 +0000

    Implement functions to test cnn model

--- models_util.py
+++ models_util.py
@@ -3,7 +3,7 @@ import torch
 from combined_model import CombinedModel
 import gpytorch
 import numpy as np
-from data_util import read_data
+from data_util import get_data_loader, read_data
 
 
 def load_combined_model(file_path):
@@ -72,10 +72,38 @@ def test_model_with_rejection(model, likelihood, test_loader, variance_threshold
     return correct, correct_variance, wrong_variance
 
 
-# def test_cnn_model(cnn_model):
-#     x_test, y_test = read_data('data/mnist-with-awgn.mat')
-#     score = cnn_model.evaluate(x_test, y_test, verbose=0)
-#     print('CNN Test accuracy:', score[1])
+def test_cnn_model(cnn_model, file_path):
+    x_test, y_test = read_data(file_path)
+    score = cnn_model.evaluate(x_test, y_test, verbose=0)
+    print('CNN Test accuracy:', score[1])
+
+
+def test_cnn_model_with_rejection(cnn_model, file_path, penalty=1):
+    x_test, y_test = read_data(file_path)
+    y_pred = cnn_model.predict(x_test)
+    correct = 0
+    for i, _ in enumerate(y_test):
+        correct += 1 if np.argmax(y_test[i]) == np.argmax(y_pred[i]) else -penalty
+    print('CNN Test Score:', correct)
+    return correct
+
+
+def compare_rejection_models(file_path, cnn_model, combined_model, likelihood):
+    cnn_scores = []
+    gp_scores = []
+
+    for penalty in range(1, 11, 2):
+        print("Penalty: {}".format(penalty))
+        test_loader = get_data_loader(file_path)
+        gp_score, _, _ = test_model_with_rejection(combined_model, likelihood, test_loader,
+                                                   variance_threshold=0.06, penalty=penalty)
+
+        cnn_score = test_cnn_model_with_rejection(cnn_model, file_path, penalty=penalty)
+        cnn_scores.append(cnn_score)
+        gp_scores.append(gp_score)
+        print()
+        print()
+    return cnn_scores, gp_scores
 
 
 def compute_variance(latent_func, mixing_weights, pred, target):
