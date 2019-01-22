3ed6172326bf957e12a5a8c48169c6b9049348d9
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-01-22 11:59:59 +0000

    Implement custom dataset for images data

---
+++ modified_mnist_dataset.py
@@ -0,0 +1,23 @@
+from torch.utils.data import Dataset
+import scipy
+from PIL import Image
+
+
+class ModifiedMnistDataset(Dataset):
+    def __init__(self, file_path, transform=None):
+        mat = scipy.io.loadmat(file_path)
+
+        self.x_test = mat['test_x']
+        self.y_test = mat['test_y']
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.x_test)
+
+    def __getitem__(self, idx):
+        sample = Image.fromarray(self.x_test[idx].reshape((28, 28)))
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, self.y_test[idx]
