270595ff5af37a7557fa29b82a9a76558f5d7662
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-01-24 02:33:00 +0000

    Update combined model to use new feature extractor

--- combined_model.py
+++ combined_model.py
@@ -12,8 +12,9 @@ class CombinedModel(gpytorch.Module):
         self.num_dim = num_dim
 
     def forward(self, x):
-        features = self.feature_extractor.forward(x.numpy().reshape(-1, 28, 28, 1))
-        features = torch.from_numpy(features)
+        # features = self.feature_extractor.forward(x.numpy().reshape(-1, 28, 28, 1))
+        features = self.feature_extractor.forward(x)
+        # features = torch.from_numpy(features)
         features = gpytorch.utils.grid.scale_to_bounds(features, self.grid_bounds[0],
                                                        self.grid_bounds[1])
         res = self.gp_layer(features)
